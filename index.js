f6aeb3db8abd040143ed07500e812638793f7d4b
Liz Thompson <thompsonel2011@gmail.com>
2021-06-06 17:24:55 -0400

    taking a break

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 // Create functions to have questions for engineer, intern, manager (not employee)
 // inquirer .prompt .... 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const managerQuestions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?',
+    },
+    {
+      type: 'input',
+      name: 'projectDescription',
+      message: 'Please provide a short description of the what, why and how of your project.',
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Please provide a link for your project.',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your email address here.',
+    }]
 
 
 
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(error) {
+        console.log('fileName:', fileName)
+        console.log('data:', data)
+        if (error) {
+            return console.log(error)
+        }
+    }
+    )}
 
-//end function with .then statement to retrieve answers from functions 
-.then(answer => {
+// Create function to initialize program 
+function init (){
+    inquirer.prompt(questions)
+        .then(function(data) {
+            writeToFile("README.md", generateMarkdown(data));
+            console.log(data)
+    }
+    )
+}
 
-})
+init();
 
 
