9c8334641661a373c3ff72d7518e8990c65dd148
Liz Thompson <thompsonel2011@gmail.com>
2021-06-07 17:05:04 -0400

    HTML attempt

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const fs = require("fs");
 const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
+const path = require("path");
+const RESULT = path.resolve(__dirname, "result");
+const html = path.join(RESULT, "index.html")
+const render = require("./src/html.js");
 
 const roleQuestion = [
   {
@@ -12,8 +16,25 @@ const roleQuestion = [
     name: "role",
     message: "Which type of team member would you like to add?",
     choices: ["Manager", "Engineer", "Intern"],
-  },
+  }
 ];
+
+function continueOn () {
+  inquirer.prompt([
+  {
+    type: "list",
+    name: "moreEmployees",
+    message: "Would you like to add another team member?",
+    choices: ["yes", "no"],
+  }]).then((answer)=> {
+    if (answer.moreEmployees == "yes") {
+      roleQuestion;
+    }
+    else {
+      generateTeam ();
+    }
+  })
+}
 
 const managerQuestions = [
   {
@@ -84,25 +105,53 @@ const internQuestions = [
   },
 ];
 
+const employeeData = [];
+
 function init() {
   inquirer.prompt(roleQuestion).then((roleAnswer) => {
-    if (roleAnswer.role === "Manager") {
-      console.log(roleAnswer.role)
+    switch (roleAnswer.role)
+    {
+    case "Manager":
       inquirer.prompt(managerQuestions).then((managerAnswers) => {
         const manager = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber);
+        employeeData.push(manager)
+        continueOn();
       });
-     } else if (roleAnswer.role === "Engineer") {
-       console.log(roleAnswer.role)
-       inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
+
+    break;
+    
+    case "Engineer":
+      inquirer.prompt(engineerQuestions).then((engineerAnswers) => {
         const engineer = new Engineer(engineerAnswers.name, engineerAnswers.id, engineerAnswers.email, engineerAnswers.gitHub);
+        employeeData.push(engineer)
+        continueOn();
       });
-     } else if (roleAnswer.role === "Intern") {
-       console.log(roleAnswer.role)
-       inquirer.prompt(internQuestions).then((internAnswers) => {
+      
+    break;
+
+    case "Intern":
+      inquirer.prompt(internQuestions).then((internAnswers) => {
         const intern = new Intern(internAnswers.name, internAnswers.id, internAnswers.email, internAnswers.school);
+        employeeData.push(intern)
+        continueOn();
+      });
+    
+    break; 
+
+    default:
+        generateTeam();
+    }
+
       });
     }
-  });
-}
-
 init();
+
+
+function generateTeam () {
+  if (!fs.existsSync(RESULT)){
+    fs.mkdirSync(RESULT)
+  }
+  else {
+    fs.writeFileSync(html, employeeData,"utf-8")
+  }
+} 
