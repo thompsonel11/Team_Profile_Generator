7bfe6d0a5e0bef29d522c8bd47f6f63a5a1b7bd0
Liz Thompson <thompsonel2011@gmail.com>
2021-06-07 10:52:52 -0400

    index.js

--- index.js
+++ index.js
@@ -1,49 +1,106 @@
-// Create functions to have questions for engineer, intern, manager (not employee)
-// inquirer .prompt .... 
+// Create functions for employee questions
+
 const inquirer = require('inquirer');
 const fs = require('fs');
+const employee = require('lib/Employee.js');
+const manager = require('lib/Manager.js');
+const engineer = require('lib/Intern.js');
+const intern = require('lib/Intern.js');
+
+const roleQuestion = [
+  {
+      type: 'list',
+      name: 'role',
+      message: "Which type of team member would you like to add?",
+      choices: ['Manager', 'Engineer', 'Intern'],
+  }
+];
 
 const managerQuestions = [
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?',
+      message: 'What is the managers name?',
     },
     {
       type: 'input',
-      name: 'projectDescription',
-      message: 'Please provide a short description of the what, why and how of your project.',
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Please provide a link for your project.',
+      name: 'id',
+      message: 'What is the managers ID?',
     },
     {
       type: 'input',
       name: 'email',
-      message: 'Please enter your email address here.',
+      message: 'What is the managers email?',
+    },
+    {
+      type: 'input',
+      name: 'number ',
+      message: 'what is the managers office phone number?',
     }]
 
+  const engineerQuestions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the engineers name?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the engineers ID?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the engineers email?',
+    },
+    {
+      type: 'input',
+      name: 'gitHub',
+      message: 'what is the engineers GitHub username?',
+    }]
 
+  const internQuestions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the interns name?',
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'What is the interns ID?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is the interns email?',
+    },
+    {
+      type: 'input',
+      name: 'gitHub',
+      message: 'what is the interns school?',
+    }]
+      
 
-// Function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, function(error) {
-        console.log('fileName:', fileName)
-        console.log('data:', data)
-        if (error) {
-            return console.log(error)
-        }
-    }
-    )}
-
-// Create function to initialize program 
+// HELP - INITIALIZE QUESTIONS AND SEND ANSWERS TO HMTL FIELDS 
 function init (){
-    inquirer.prompt(questions)
-        .then(function(data) {
-            writeToFile("README.md", generateMarkdown(data));
-            console.log(data)
+  inquirer.prompt(roleQuestion)
+    .then((roleAnswer) => {
+        if (roleAnswer.role === 'Manager')  {
+          inquirer.prompt(managerQuestions)
+            .then((managerAnswers) => {
+              const manager = new Manager
+            }
+            )
+        }
+        else if (roleAnswer.role === 'Engineer') {
+
+        }
+        else if (roleAnswer.role === 'Intern') {
+
+        }
+
     }
     )
 }
